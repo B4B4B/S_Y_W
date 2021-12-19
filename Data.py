de951878b0276b052ffcf65309f3300299763dec
botati <84269812+botati@users.noreply.github.com>
2021-12-19 15:18:08 +0300

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,52 +4,38 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     # Start Message
     START = """
-- مرحبا بك في بوت جمثون
-
-من هذا البوت يمكنك استخراج كود تيرمكس/بايروجرام بسهولة وبسرعة و بدون مشاكل اختر امر الاستخراج في الاسفل واكمل العملية  ، 
-
-@JMTHON
+- اهلا عزيزي {}
+أهلا بك {}
+يعمل هذا البوت لمساعدتك بطريقة سهله للحصول على كود تيرمكس او كود بايروجرام
     """
 
     # Home Button
-    home_buttons = [
-        [InlineKeyboardButton(" • بدء الاستخراج  •", callback_data="generate")],
-        [InlineKeyboardButton(text="• الواجهة الرئيسية •", callback_data="home")]
-    ]
+    home_buttons = [        [InlineKeyboardButton("- ابدا بأستخراج كود .", callback_data="generate")],        [InlineKeyboardButton(text="- رجوع .", callback_data="home")]    ]
 
-    generate_button = [
-        [InlineKeyboardButton("• بدء الاستخراج  •", callback_data="generate")]
-    ]
+    generate_button = [        [InlineKeyboardButton("- ابدا بأستخراج كود .", callback_data="generate")]    ]
 
     # Rest Buttons
-    buttons = [
-        [
-            InlineKeyboardButton("• بدء الاستخراج  •", callback_data="generate")
-            InlineKeyboardButton("• كيفية الاستخدام  •", callback_data="help"),
-        ],
-        [InlineKeyboardButton(" • قناة السورس •", url="https://t.me/JMTHON")],
-    ]
+    buttons = [        [InlineKeyboardButton("- ابدا بأستخراج كود .", callback_data="generate")],        [InlineKeyboardButton("- قناه السورس .", url="https://t.me/jmthon")],
+        [            InlineKeyboardButton("- طريقة الاستخدام ؟ .", callback_data="help"),            InlineKeyboardButton("- حول البوت .", callback_data="about")        ],]
 
     # Help Message
-    HELP = """ 
-✨ الاوامر المتوفرة التي يمكنك مساعدة استخدام البوت بسلاسة هي : ✨
+    HELP = """
+** - الاوامر المتوفرة . **
 
-/about 
-لعـرض معلومـات البـوت
-/help 
- لعـرض اوامـر البوت
-/start 
- لتشغـيل البـوت
-/generate 
-لبـدء استخـراج كود تيرمكس/بايروجرام
+/about - معلومات عن البوت
+/help - هذه الرسالة
+/start - تشغيل البوت 
+/generate - البدا بأستخراج جلسة جديده
+/cancel - الغاء العملية
+/restart - عملية الالغاء
 """
 
     # About Message
     ABOUT = """
-بوت استخراج كود تيرمكس/بايروجرام
+**- حول هذا البوت .** 
 
-وهـو عبـارة عن بوت بسيط للمساعـدة في استخـراج كـود تيرمكس/بايروجرام بسهولة وبأمان تام ومساعدك في عمليه تنصيب سورس جمثون 
+- بوت يساعدك على استخراج كوك تيرمكس و بايروجرام .
 
-قناة السورس  : 
-@Jmthon
->    """
+- قروب الدعم للمساعدة : [Support](https://t.me/jmthon)
+
+    """
